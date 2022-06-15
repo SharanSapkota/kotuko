79f8f89de73816a56c2499749b660398610301be
sharan <sharan@botsplash.com>
2022-06-15 10:45:07 +0545

    Fix linting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 const express   = require('express');
 const app       = express()
-const endPoints = require('./routes/getRequest.ts')
+const endPoints = require('./routes/index.ts')
 const dotenv    = require('dotenv').config({ path: '../.env' }); 
 
 app.use(endPoints)
 app.use(express.json())
-console.log(dotenv)
-const port = process.env.PORT
 
-app.listen(port || 4200, () => {
+const port      = process.env.PORT || 4200;
+
+app.listen( port, () => {
     console.log(`Server started on port ${port}`)
 })
