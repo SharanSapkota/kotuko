ab57f4133d832a91c74947b813b7abf6a8601237
sharan <sharan@botsplash.com>
2022-06-15 08:23:50 +0545

    Define type for guardian response

---
+++ src/interface/IGuardianResponse.interface.ts
@@ -0,0 +1,42 @@
+ export interface IGuardian {
+    status: string,
+    userTier: string,
+    total: number,
+    startIndex: number,
+    pageSize: number,
+    currentPage: number,
+    pages: number,
+    edition: IEdition,
+    section: ISection,
+    results: Array<IResult>,
+}
+
+interface IEdition {
+    id: string,
+    webTitle: string,
+    webUrl: string,
+    apiUrl: string,
+    code: string
+}
+
+interface IResult {
+    id: string,
+    type: string,
+    sectionId: string,
+    sectionName: string,
+    webPublicationData: string,
+    webTitle: string,
+    webUrl: string,
+    apiUrl: string,
+    isHosted: string,
+    pillarId: string,
+    pillarName: string
+}
+
+interface ISection {
+    id: string,
+    webTitle: string,
+    webUrl: string,
+    apiUrl: string,
+    editions: any
+}
