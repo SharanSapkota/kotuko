f383173d24ddefa5c3f28f08939dc560bd125061
sharan <sharan@botsplash.com>
2022-06-15 14:25:21 +0545

    Fix linting

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -8,15 +8,15 @@ require('dotenv').config('../../.env')
 exports.getArticle = async (req: any, res: any) => {
     if(!caseValidation(req.params.article)) {
         res.status(202).send({message: 'Please recheck your section case!'})
-    } 
-    else {
-    try{
-        const guardianRes = await guardianAPI(req.params.article)
-        const rssData = await generateRss(guardianRes.data.response);
-        res.set('content-type', 'text/xml');    
-        res.status(200).send(rssData);
-} catch (e: any) {
-    res.status(422).json({ 'Error Message' : e.message});}
-}
+    } else {
+        try{
+            const guardianRes = await guardianAPI(req.params.article)
+            const rssData = await generateRss(guardianRes.data.response);
+            res.set('content-type', 'text/xml');    
+            res.status(200).send(rssData);
+        } catch (e: any) {
+        res.status(422).json({ 'Error Message' : e.message});
+        }
+    }
 }
 
--- src/services/guardianAPI.ts
+++ src/services/guardianAPI.ts
@@ -1,10 +1,8 @@
 const axios = require('axios');
 
-const guardianAPI = async (article: string) => {
-    return await axios.get(`https://content.guardianapis.com/${article}`,{
+const guardianAPI = async (article: string) =>  await axios.get(`https://content.guardianapis.com/${article}`,{
     params: { 'api-key' : process.env.API_KEY}
 }
 )
-}
 
-  export default guardianAPI;
+export default guardianAPI;
