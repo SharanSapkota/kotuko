215a532f20f71c0b83a1a7e9ac5d1b3ee47fcc22
sharan <sharan@botsplash.com>
2022-06-16 13:30:06 +0545

    Fix linting and add log to response

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -1,21 +1,23 @@
 import guardianAPI from "../services/guardianAPI"
 import caseValidation from "../utils/caseValidation"
+import logger from "../utils/logger"
 import generateRss  from "../utils/rss"
 
-const axios = require('axios')
-require('dotenv').config('../../.env')
 
 exports.getArticle = async (req: any, res: any) => {
+
     if(!caseValidation(req.params.article)) {
-        res.status(202).send({message: 'Please recheck your section case!'})
+        res.status(202).send({ message: 'Please recheck your section case! '})
     } else {
         try{
             const guardianRes = await guardianAPI(req.params.article)
-            const rssData = await generateRss(guardianRes.data.response);
-            res.set('content-type', 'text/xml');    
+            const rssData     = await generateRss(guardianRes.data.response);
+            logger.info(guardianRes.data.response)
+            res.set('content-type', 'text/xml');   
             res.status(200).send(rssData);
         } catch (e: any) {
-        res.status(422).json({ 'Error Message' : e.message});
+            logger.error(e)
+            res.status(422).json({ 'Error Message' : e.message});
         }
     }
 }
