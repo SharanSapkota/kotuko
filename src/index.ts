ef9b0d139be2f1adf1ee347e7f9574ad12a7992b
sharan <sharan@botsplash.com>
2022-06-16 13:29:32 +0545

    Add loggerMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import { loggerMidleware } from "./middleWare/loggerMiddleware";
+
 const express   = require('express');
 const app       = express()
 const endPoints = require('./routes/index.ts')
 const dotenv    = require('dotenv').config({ path: '../.env' }); 
 
 app.use(endPoints)
-app.use(express.json())
+app.use(express.json());
 
-const port      = process.env.PORT || 4200;
+app.use(loggerMidleware)
+
+const port      = process.env.PORT || 4100;
 
 app.listen( port, () => {
     console.log(`Server started on port ${port}`)
